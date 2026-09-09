fc55a40b7facccac1d8ed5deb7da504bcebab26f
Nashte13 <nashmwangi26@gmail.com>
2026-09-09 15:39:58 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,6 @@
 import { useFeedbackStore } from "./store";
 
 
-const Statistics = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad
-  const mean = total === 0 ? 0 : total / 3
-  const positive = total === 0 ? 0 : ((good / total) * 100).toFixed(1)
-
-  if (total === 0) {
-    return <p>No feedback given</p>;
-  }
-
-  return (
-    <table>
-        <tbody>
-            <StatisticLine text="good" value={good} />
-            <StatisticLine text="neutral" value={neutral} />
-            <StatisticLine text="bad" value={bad} />
-            <StatisticLine text="all" value={total} />
-            <StatisticLine text="average" value={mean} />
-            <StatisticLine text="positive" value={positive + "%"} />
-        </tbody>
-    </table>
-  );
-};
-
 const App = () => {
   const { good, neutral, bad, incrementGood, incrementNeutral, incrementBad } = useFeedbackStore();
 
